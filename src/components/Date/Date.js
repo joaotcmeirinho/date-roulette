dbfd6dbaa958ce111db8d756f69da59a5a03d46b
jmeiradas <jmeiradas@gmail.com>
2022-03-04 00:23:11 +0000

    finished with mobile version

--- src/components/Date/Date.js
+++ src/components/Date/Date.js
@@ -6,16 +6,14 @@ const Date = ({ result }) => {
     window.location.reload(false);
   };
   return (
-    <div className="result-bg">
-      <div className="result-ctn">
-        <h1 className="result-title">What about?</h1>
-        <div className="result-box">
-          <h2 className="result-name">{result.name}</h2>
-        </div>
-        <button className="result-btn" onClick={onClick}>
-          Try again
-        </button>
+    <div className="result-ctn">
+      <h1 className="result-title">What about?</h1>
+      <div className="result-box">
+        <h2 className="result-name">{result.name}</h2>
       </div>
+      <button className="result-btn" onClick={onClick}>
+        Try again
+      </button>
     </div>
   );
 };
--- src/components/DateForm/DateForm.js
+++ src/components/DateForm/DateForm.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import Date from "../Date/Date";
 import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
+import "./DateForm.css";
+import heart from "../../assets/heart.png";
+import { IoIosArrowBack } from "react-icons/io";
+import { IconContext } from "react-icons/lib";
 
 const DateForm = () => {
   const [dates, setDates] = useState([]);
   const [filter, setFilter] = useState([]);
   const [filteredResults, setFilteredResults] = useState([]);
+  const [active, setActive] = useState(false);
   const [isButtonClicked, setIsButtonClicked] = useState(false);
   const [result, setResult] = useState();
 
@@ -35,7 +41,6 @@ const DateForm = () => {
   };
 
   const showDates = (e) => {
-    e.preventDefault();
     if (filter.length) {
       let data = filteredResults;
       for (let i = 0; i < filter.length; i++) {
@@ -56,75 +61,115 @@ const DateForm = () => {
     setIsButtonClicked(true);
   };
 
+  const handleSubmit = () => {
+    setActive(true);
+
+    setTimeout(showDates, 1000);
+  };
+
   return (
-    <div>
-      {!isButtonClicked ? (
-        <>
-          <form>
-            <label>Romantic</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="romantic"
+    <div className="form-bg">
+      <div className="form-ctn">
+        <div className="go-back-btn">
+          <NavLink to="/">
+            <IconContext.Provider value={{ size: "20px", color: "black" }}>
+              <IoIosArrowBack />
+            </IconContext.Provider>
+          </NavLink>
+        </div>
+
+        {!isButtonClicked ? (
+          <>
+            <h1 className="form-title">Date Roulette</h1>
+            <img
+              className={!active ? "heart-img" : "heart-img-2"}
+              src={heart}
             />
-            <label>Adventure</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="adventure"
-            />
-            <label>At home</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="home"
-            />
-            <label>Cheap</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="cheap"
-            />
-            <label>Alternative</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="alternative"
-            />
-            <label>Long Distance</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="long_distance"
-            />
-            <label>Expensive</label>
-            <input
-              onChange={(e) => {
-                handleFilter(e);
-              }}
-              type="checkbox"
-              value="expensive"
-            />
-          </form>
-          <button onClick={showDates}>Give me Dates</button>
-        </>
-      ) : (
-        <>
-          <Date result={result} />
-        </>
-      )}
+            <div className="input-fields-ctn">
+              <h4 className="filters">Filters:</h4>
+              <div className="input-fields-body">
+                <div className="input-field">
+                  <label>Romantic</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="romantic"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Adventure</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="adventure"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>At home</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="home"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Cheap</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="cheap"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Alternative</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="alternative"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Long Distance</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="long_distance"
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Expensive</label>
+                  <input
+                    onChange={(e) => {
+                      handleFilter(e);
+                    }}
+                    type="checkbox"
+                    value="expensive"
+                  />
+                </div>
+              </div>
+            </div>
+            <button className="form-btn" onClick={handleSubmit}>
+              Give me Dates
+            </button>
+          </>
+        ) : (
+          <>
+            <Date result={result} />
+          </>
+        )}
+      </div>
     </div>
   );
 };
